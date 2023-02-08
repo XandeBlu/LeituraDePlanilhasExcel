1994288e39ead3e399eb1b2467a7c45e66c2d982
XandeBlu <92054535+XandeBlu@users.noreply.github.com>
2023-02-08 16:48:31 -0300

    Descrição
    Adicionei uma descrição para melhorar a compreensão do código, falta adicionar a função de enviar um sms somente.

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ import pandas as pd
 
 Lista_meses = ['janeiro','fevereiro','março','abril','maio','junho']
 for mes in Lista_meses:
-    print(mes)
-    tabela_venda = pd.read_excel (mes +'.xlsx')
-    print (tabela_venda)
+    tabela_venda = pd.read_excel (f'{mes}.xlsx')
+    if (tabela_venda['Vendas'] > 55000).any():
+        vendedor = tabela_venda.loc[tabela_venda['Vendas'] > 55000,'Vendedor'].values[0]
+        vendas = tabela_venda.loc[tabela_venda['Vendas'] > 55000,'Vendas'].values[0]
+        print (f'No mês de {mes}, o vendedor {vendedor}, bateu a marca de {vendas} e ganhou o benefício')
+
+#Primeiramente, criei um array chamado lista_meses, depois, criei uma estrutura de repetição para passar cada mês um a um.
+#Após isso, criei uma variavel para receber esses valores, se chama tabela_venda, essa varíavel precisa da biblioteca pandas, que abreviei para pd para funcionar e do método read_excel.
+#apos isso, verifico se existe qualquer valor na coluna vendas das planilhas maior que 55000, se tiver, cria-se uma variável vendedor e uma vendas, que receberá o nome do vendedor e o valor das vendas respctivamente.
+#para saber qual linha da planilha tem um valor maior que 55000, eu uso o método loc(linha,coluna), para localizar a linha.!Importante, o Método Loc retorna uma tabela como resultado,
+#então é necessário usar o values[0], para retornar somente o valor. Após isso, basta printar o resultado.
